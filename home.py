9a722b587a557f8e9efc77ecf570dc52b9c52b69
franciscocribeiro <126168501+franciscocribeiro@users.noreply.github.com>
2023-02-24 15:14:07 +0000

    Add files via upload

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.title("Clothing Product Recommendation")
+st.subheader("Main Page")
+st.markdown("Select a product category from the sidebar")
+    
+import streamlit as st
+
+from streamlit.components.v1 import html
+
